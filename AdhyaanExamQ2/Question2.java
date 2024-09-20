9fc37f8654d034a82e8c17a0fce6591192915752
Ajhar Tamboli <152916829+AjharTamboli08@users.noreply.github.com>
2024-09-20 12:15:08 +0530

    Update Question2.java

--- AdhyaanExamQ2/Question2.java
+++ AdhyaanExamQ2/Question2.java
@@ -1,4 +1,34 @@
 package AdhyaanExamQ2;
-
+import java.util.Scanner;
 public class Question2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        printFactors(number);
+
+        scanner.close();
+    }
+
+    public static void printFactors(int num) {
+        boolean hasFactors = false;
+
+        System.out.print("Factors of " + num + " (excluding 1 and " + num + "): ");
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+                hasFactors = true;
+            }
+        }
+
+        if (!hasFactors) {
+            System.out.println("none, the number is prime or has factors only 1 and itself.");
+        } else {
+            System.out.println();
+        }
+    }
 }
